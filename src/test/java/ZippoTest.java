1a2d9b7d77c971d2b863e887382ec176fea19670
TechnoStudy <ismettechno@gmail.com>
2023-10-25 18:44:22 +0100

    test3 hemcrest POM xml e eklendi

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,6 +1,8 @@
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class ZippoTest {
 
@@ -30,6 +32,41 @@ public class ZippoTest {
                 .statusCode(200) // test kısmı olduğundan assertion status code 200 mü
         ;
     }
+
+    @Test
+    public void contentTypeTest(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body() // dönen body json data , log().all() : gidip gelen her şey
+                .statusCode(200) // test kısmı olduğundan assertion status code 200 mü
+                .contentType(ContentType.JSON)  // dönen datanın tipi JSON mı
+        ;
+    }
+
+    @Test
+    public void test3(){
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .statusCode(200)  // assertion
+                .body("country", equalTo("United States")) //assertion
+        ;
+    }
+
+
+
+
+
 }
 
 
