4ef386eba0ba4e6e0aa7f6a79603613982f54ad7
TechnoStudy <ismettechno@gmail.com>
2023-10-31 17:30:17 +0000

    getZipCode

--- src/test/java/_05_PathAndJsonPath.java
+++ src/test/java/_05_PathAndJsonPath.java
@@ -1,4 +1,10 @@
+import Model.Location;
+import Model.Place;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 
 public class _05_PathAndJsonPath {
@@ -21,7 +27,7 @@ public class _05_PathAndJsonPath {
     }
 
     @Test
-    public void extractingJosnPath()
+    public void extractingJsonPath()
     {
         int postCode=  // int e dönüşüm de JsonPath yönteminde hata almadık
                 given()
@@ -38,5 +44,54 @@ public class _05_PathAndJsonPath {
         System.out.println("postCode = " + postCode);
     }
 
+    @Test
+    public void getZipCode()
+    {
+        Response response=
+         given()
+
+                 .when()
+                 .get("http://api.zippopotam.us/us/90210")
+
+                 .then()
+                 .log().body()
+                 .extract().response()
+        ;
+
+        Location locationPathAs= response.as(Location.class); // Bütün classları yazmak zorundasın
+        System.out.println("locationPathAs.getPlaces() = " + locationPathAs.getPlaces());
+
+        List<Place> places= response.jsonPath().getList("places", Place.class);
+        System.out.println("places = " + places);// nokta atışı istediğimiz nesneyi aldık.
+
+        // Daha önceki örneklerde (as) Clas dönüşümleri için tüm yapıya karşılık gelen
+        // gereken tüm classları yazarak dönüştürüp istediğimiz elemanlara ulaşıyorduk.
+
+        // Burada ise(JsonPath) aradaki bir veriyi clasa dönüştürerek bir list olarak almamıza
+        // imkan veren JSONPATH i kullandık.Böylece tek class ile veri alınmış oldu
+        // diğer class lara gerek kalmadan
+
+        // path : class veya tip dönüşümüne imkan veremeyen direk veriyi verir. List<String> gibi
+        // jsonPath : class dönüşümüne ve tip dönüşümüne izin vererek , veriyi istediğimiz formatta verir.
+    }
+
+
+
+    // https://gorest.co.in/public/v1/users  endpointte dönen Sadece Data Kısmını POJO
+    // dönüşümü ile alarak yazdırınız.
+
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
