5a79629372203d5833e404e9de2e881eb5b6300a
TechnoStudy <ismettechno@gmail.com>
2023-10-31 18:28:29 +0000

    createUser

---
+++ src/test/java/GoRest/_06_GoRestUsersTest.java
@@ -0,0 +1,69 @@
+package GoRest;
+
+import com.github.javafaker.Faker;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class _06_GoRestUsersTest {
+
+
+//    https://gorest.co.in/public/v2/users   POST
+//
+//    giden body
+//    {
+//        "name":"{{$randomFullName}}",
+//            "gender":"male",
+//            "email":"{{$randomEmail}}",
+//            "status":"active"
+//    }
+//
+//    Authorization
+//    Bearer 787c83039875452ce6a32a7b73e7a10c1d4443273522652da0f13d5e76a27713
+//
+//
+//    dönüşte 201
+//    id extract
+
+      Faker randomUreteci=new Faker();
+      int userID=0;
+
+      @Test
+      public void createUser(){
+
+            String rndFullName= randomUreteci.name().fullName();
+            String rndEmail= randomUreteci.internet().emailAddress();
+
+            userID=
+            given() // giden body, token, contentType
+                    .header("Authorization","Bearer 787c83039875452ce6a32a7b73e7a10c1d4443273522652da0f13d5e76a27713")
+                    .body("{\"name\":\""+rndFullName+"\", \"gender\":\"male\", \"email\":\""+rndEmail+"\", \"status\":\"active\"}") // giden body
+                    .contentType(ContentType.JSON)
+
+                    .when()
+                    .post("https://gorest.co.in/public/v2/users")
+
+                    .then()
+                    .log().body()
+                    .statusCode(201)
+                    .extract().path("id");
+            ;
+            System.out.println("userID = " + userID);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
