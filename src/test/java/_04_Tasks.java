0981913060ff0964d011006e0e0e573c444c9ed5
TechnoStudy <ismettechno@gmail.com>
2023-10-31 16:25:29 +0000

    task2

--- src/test/java/_04_Tasks.java
+++ src/test/java/_04_Tasks.java
@@ -1,4 +1,5 @@
 import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -15,7 +16,7 @@ public class _04_Tasks {
      */
 
     @Test
-    public void task3() {
+    public void task1() {
 
         given()
 
@@ -29,6 +30,50 @@ public class _04_Tasks {
         ;
 
     }
+
+    /**
+     * Task 2
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * expect content type JSON
+     *a) expect response completed status to be false(hamcrest)
+     *b) extract completed field and testNG assertion(testNG)
+     */
+
+    @Test
+    public void task2(){
+        // a)
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("completed", equalTo(false))
+        ;
+
+        // b)
+        boolean completed=
+        given()
+
+                .when()
+                .get("https://jsonplaceholder.typicode.com/todos/2")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().path("completed")
+        ;
+        Assert.assertFalse(completed);
+    }
+
+
+
+
+
+
 
 
 }
